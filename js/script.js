f8bef54c68932cc4dbc6430b82a9149ec28c3993
Daniel <154093698+Daniel-Cafe@users.noreply.github.com>
2024-01-16 14:52:45 -0300

    att

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function mostrarDiv() {
+    var div = document.getElementById('menu-mobile');
+    div.style.display = 'block';
+}
+
+function esconderDiv() {
+    var div = document.getElementById('menu-mobile');
+    div.style.display = 'none';
+}
+
+function mostrarOverlay() {
+    var div = document.getElementById('overlay-menu');
+    div.style.display = 'block';
+}
+
+function esconderOverlay() {
+    var div = document.getElementById('overlay-menu');
+    div.style.display = 'none';
+}
